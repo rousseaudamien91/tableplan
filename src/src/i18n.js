f6a24e03bea9fc7d4119b8311c80260974380c02
rousseaudamien91 <rousseau.damien.91@gmail.com>
2026-03-30 23:27:16 +0200

    Delete src/src/i18n.js

--- src/src/i18n.js
+++
@@ -1,9 +0,0 @@
-import pricing from "../locales/fr/pricing.json";
-import reviewsPublic from "../locales/fr/reviewsPublic.json";
-
-export const fr = {
-  ...autresNamespaces,
-  superadmin,
-  pricing,
-  reviewsPublic
-};
