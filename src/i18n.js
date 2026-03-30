3eefdd7ab5c63a0e12f6a4cf5dc7586caf88e49d
rousseaudamien91 <rousseau.damien.91@gmail.com>
2026-03-30 23:27:37 +0200

    Add files via upload

---
+++ src/i18n.js
@@ -0,0 +1,9 @@
+import pricing from "../locales/fr/pricing.json";
+import reviewsPublic from "../locales/fr/reviewsPublic.json";
+
+export const fr = {
+  ...autresNamespaces,
+  superadmin,
+  pricing,
+  reviewsPublic
+};
