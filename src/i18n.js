a54842d48932a58ec68f7bf1818bf37f95a623dc
rousseaudamien91 <rousseau.damien.91@gmail.com>
2026-03-31 20:02:51 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,6 @@ import pricing from "./locales/fr/pricing.json";
 import reviewsPublic from "./locales/fr/reviewsPublic.json";
 
 export const fr = {
-  ...autresNamespaces,
-  superadmin,
   pricing,
   reviewsPublic
 };
