4bc4dfc9d9b4caacb10578958decfa840bc9b182
rousseaudamien91 <rousseau.damien.91@gmail.com>
2026-03-31 22:27:17 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
+/* eslint-disable */
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// ⚠️ Remplace chaque valeur par celles de ton projet Firebase
 const firebaseConfig = {
-  apiKey: "TA_VRAIE_API_KEY",
-  authDomain: "ton-projet.firebaseapp.com",
-  projectId: "ton-projet",
-  storageBucket: "ton-projet.appspot.com",
-  messagingSenderId: "xxxxxxx",
-  appId: "1:xxxxxxx:web:xxxxxxx"
+  apiKey: "AIzaSyAtDeOELj6om9-9mEdKVRbXMPG-i13szSo",
+  authDomain: "tableplan-c4a6b.firebaseapp.com",
+  projectId: "tableplan-c4a6b",
+  storageBucket: "tableplan-c4a6b.appspot.com",
+  messagingSenderId: "659977410619",
+  appId: "1:659977410619:web:ae761fbf149f923990641e"
 };
 
+// Initialisation
 const app = initializeApp(firebaseConfig);
 
+// Services
 export const db = getFirestore(app);
 export const auth = getAuth(app);
