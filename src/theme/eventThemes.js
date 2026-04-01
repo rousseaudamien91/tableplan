980f5462b0a2a63d32aff04187af0de0dd507ec7
rousseaudamien91 <rousseau.damien.91@gmail.com>
2026-04-01 17:50:14 +0200

    Create eventThemes.js

---
+++ src/theme/eventThemes.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+export const EVENT_THEMES = {
+  mariage: { label: "Mariage", icon: "💍", color: "#C9973A" },
+  gala: { label: "Gala / Soirée", icon: "🥂", color: "#8B7EC8" },
+  anniversaire: { label: "Anniversaire", icon: "🎂", color: "#E8845A" },
+  conference: { label: "Conférence", icon: "🎤", color: "#4A9B7F" },
+  autre: { label: "Autre", icon: "🎊", color: "#C9973A" },
+};
